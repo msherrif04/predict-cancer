b38cf0cb2f615da39375b99e3097de6f729a187b
Sherrif <msherrif04@gmail.com>
2024-03-30 14:00:50 +0000

    complete dashboard

--- model/main.py
+++ model/main.py
@@ -1,9 +1,41 @@
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+import pickle
+
+
+def create_model(data):
+    X = data.drop(["diagnosis"], axis=1)
+    y = data["diagnosis"]
+
+    # scale the data
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+
+    # splitting the data into test and train sets
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    # training the model
+    regressor = LogisticRegression()
+    regressor.fit(X_train, y_train)
+
+    # testing the model
+    y_pred = regressor.predict(X_test)
+    print("Accuracy of the model: ", accuracy_score(y_test, y_pred))
+    print("Classification report: ", classification_report(y_test, y_pred))
+
+    return regressor, scaler
 
 
 def get_clean_data():
-    data = pd.read_csv("/data/data.csv")
-    print(data.head())
+    data = pd.read_csv("data/data.csv")
+    data = data.drop(["Unnamed: 32", "id"], axis=1)
+    data["diagnosis"] = data["diagnosis"].map({"M": 1, "B": 0})
+
     return data
 
 
@@ -11,7 +43,12 @@ def main():
     # create the model
     data = get_clean_data()
 
-    # evaluate the model
+    model, scaler = create_model(data)
+
+    with open("model/model.pkl", "wb") as f:
+        pickle.dump(model, f)
+    with open("model/scaler.pkl", "wb") as f:
+        pickle.dump(scaler, f)
 
 
 if __name__ == "__main__":
